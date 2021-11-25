0241d602044678e025e100b6e1682522a264a96e
Bryan Monsalvatge <bryan@Bryans-MacBook-Pro.local>
2021-11-25 00:20:31 -0500

    customizing and setting up random configs

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,32 +1,31 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Blog by Bryan Monsalvatge',
+  author: 'Bryan Monsalvatge',
+  headerTitle: 'Bryan Monsalvatge\'s Blog',
+  description: 'A blog for mostly tech and some miscellaneous topics',
   language: 'en-us',
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://bryanmonsalvatge.com',
+  siteRepo: 'https://github.com/BMonsalvatge/bryanmonsalvatge.com',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'BryanMonsalvatge@gmail.com',
+  github: 'https://github.com/BMonsalvatge',
+  twitter: 'https://twitter.com/BMonsalvatge',
+  youtube: 'https://www.youtube.com/c/bmonsalvatge',
+  linkedin: 'https://www.linkedin.com/in/bryan-monsalvatge',
   locale: 'en-US',
   analytics: {
     // supports plausible, simpleAnalytics or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: false, // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'UA-144231222-1', // e.g. UA-000000-2 or G-XXXXXXX
   },
-  newsletter: {
-    // supports mailchimp, buttondown, convertkit
-    // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
-  },
+  // newsletter: {
+  //   // supports mailchimp, buttondown, convertkit
+  //   // Please add your .env file and modify it according to your selection
+  //   provider: 'buttondown',
+  // },
   comment: {
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
@@ -52,22 +51,22 @@ const siteMetadata = {
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
     },
-    utterancesConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://utteranc.es/
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '', // supported options: pathname, url, title
-      label: '', // label (optional): Comment 💬
-      // theme example: github-light, github-dark, preferred-color-scheme
-      // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
-      theme: '',
-      // theme when dark mode
-      darkTheme: '',
-    },
-    disqusConfig: {
-      // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
-      shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
-    },
+    // utterancesConfig: {
+    //   // Visit the link below, and follow the steps in the 'configuration' section
+    //   // https://utteranc.es/
+    //   repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+    //   issueTerm: '', // supported options: pathname, url, title
+    //   label: '', // label (optional): Comment 💬
+    //   // theme example: github-light, github-dark, preferred-color-scheme
+    //   // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
+    //   theme: '',
+    //   // theme when dark mode
+    //   darkTheme: '',
+    // },
+    // disqusConfig: {
+    //   // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
+    //   shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
+    // },
   },
 }
 
