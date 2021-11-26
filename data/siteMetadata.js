0a146d9239a664e3aa08542e25f43eb8d41251d6
Bryan Monsalvatge <bryan@Bryans-MacBook-Pro.local>
2021-11-26 00:06:28 -0500

    updating social banner

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -8,7 +8,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/BMonsalvatge/bryanmonsalvatge.com',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/logo.png',
   email: 'BryanMonsalvatge@gmail.com',
   github: 'https://github.com/BMonsalvatge',
   twitter: 'https://twitter.com/BMonsalvatge',
